80a5e8eb1bf2af0768852e46095d1eedbd6c9660
pradHN1009 <pradyumnahn@gmail.com>
2023-10-20 23:42:25 +0530

    Added ability to dynamically loop through news articles and render them

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,29 +2,27 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-  render() {
-    let title = "Headlines"
-    let desc = "Some random nonsense about headlines"
-    let data = require("../NewsOutput.json")
+
+data = require("../NewsOutput.json")
+constructor(){
+    super();
+    this.state = {
+        newsArticles : this.data,
+        loading : false
+    }
+}
+render() {
+    // let data = {}
+    // fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2").then(response => response.json()).then(newsData => this.data = newsData)
     return (
       <div className="container my-3 mx-3">
         <div className="row gx-3">
-            <div className="col-sm-12 col-md-4 col-lg-3">
-            <NewsItem title = {data.articles[0].title} description = {data.articles[0].description} imgUrl = {data.articles[0].urlToImage} />
+            {this.state.newsArticles.articles.map((data)=>{
+                return <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <NewsItem key = {data.url} title = {data.title} description = {data.description} imgUrl = {data.urlToImage} />
+                </div>
+            })}
             </div>
-            <div className="col-sm-12 col-md-4 col-lg-3">
-            <NewsItem title = {data.articles[1].title} description = {data.articles[1].description} imgUrl = {data.articles[1].urlToImage}/>
-            </div> 
-            <div className="col-sm-12 col-md-4 col-lg-3">
-            <NewsItem title = {data.articles[2].title} description = {data.articles[2].description} imgUrl = {data.articles[2].urlToImage}/>
-            </div> 
-            <div className="col-sm-12 col-md-4 col-lg-3">
-            <NewsItem title = {data.articles[3].title} description = {data.articles[3].description} imgUrl = {data.articles[4].urlToImage}/>
-            </div>
-            <div className="col-sm-12 col-md-4 col-lg-3">
-            <NewsItem title = {data.articles[4].title} description = {data.articles[4].description} imgUrl = {data.articles[4].urlToImage}/>
-            </div> 
-        </div>
      </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
 
-    let {title,description,imgUrl} = this.props;
+    let {key,title,description,imgUrl} = this.props;
     return (
       <div className = "my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "100%"}}>
             <img src={imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="/" className="btn btn-sm btn-primary">Read more</a>
+                <a href={key} className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
