7c5d7aecc237f6ebce1e109865e28cb600873cfe
pradHN1009 <pradyumnahn@gmail.com>
2023-10-21 19:38:51 +0530

    Added boundary conditions to pagination

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,29 +8,31 @@ constructor(){
     this.state = {
         newsArticles : [],
         loading : false,
-        page:1
+        page:1,
+        pageSize:20,
+        totalResults : 0
     }
 }
 previousClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page-1}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page-1}&pageSize=${this.state.pageSize}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
     this.setState({newsArticles : data.articles, page : this.state.page-1})
 }
 nextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page+1}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page+1}&pageSize=${this.state.pageSize}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
     this.setState({newsArticles : data.articles, page : this.state.page+1})
 }
 async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page}&pageSize=${this.state.pageSize}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
-    this.setState({newsArticles : data.articles}) // this also works - this.setState(this.state.newsArticles = data.articles);
+    this.setState({newsArticles : data.articles, totalResults : data.totalResults}) // this also works - this.setState(this.state.newsArticles = data.articles);
 }
 render() {
     return (
@@ -44,7 +46,7 @@ render() {
             </div>
         <div className="container my-3 d-flex justify-content-between">
         <button type="button" disabled = {this.state.page<=1?true:false} class="btn btn-primary" onClick = {this.previousClick}>&larr; Previous</button>
-        <button type="button" class="btn btn-primary" onClick = {this.nextClick}>Next &rarr;</button>
+        <button type="button" disabled = {this.state.page+1>Math.ceil(this.state.totalResults/this.state.pageSize)?true:false} class="btn btn-primary" onClick = {this.nextClick}>Next &rarr;</button>
         </div>
      </div>
     )
