436feabf25a3bb283444089adc9254fd1857c79d
pradHN1009 <pradyumnahn@gmail.com>
2023-10-24 22:18:10 +0530

    Added infinte scroll funcitonality to the app

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
 static defaultProps = {
@@ -21,9 +23,10 @@ constructor(props){
         loading : false,
         page:1,
         pageSize:20,
-        totalResults : 0
+        totalResults : 0,
+        hasMore : true
     }
-    document.title = `Newstle - ${this.capitalize(this.props.category)}`
+    document.title = `Newstle - ${this.capitalize(this.props.category) }`
 }
 
 capitalize = (word) => {
@@ -36,14 +39,13 @@ updateNews = async () => {
     let data = await response.json();
     this.setState({newsArticles : data.articles, totalResults : data.totalResults, loading : false})
 }
-previousClick = async () => {
-    this.setState({page:this.state.page - 1})
-    this.updateNews()
-}
-
-nextClick = async () => {
-    this.setState({page:this.state.page + 1})
-    this.updateNews()
+fetchMoreData = async () => {
+    this.setState({page : this.state.page+1 })
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=05edbb607b1a4bd481fef3f953a611d2&page=${this.state.page}&pageSize=${this.state.pageSize}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data)
+    this.setState({newsArticles : this.state.newsArticles.concat(data.articles), totalResults : data.totalResults, hasMore : this.state.newsArticles.length < this.state.totalResults})
 }
 async componentDidMount(){
     this.updateNews()
@@ -51,23 +53,27 @@ async componentDidMount(){
 render() {
     const {mode, category} = this.props;
     return (
-      <div className="container my-3 mx-3">
+      <>
         <div className={`container my-3 text-${mode === "light" ? "dark" : "light"}`}>
             <h1 className= "text-center" style={{margin : "35px"}}>Top {this.capitalize(category)} Headlines</h1>
         </div>
-        {this.state.loading && <Spinner/>}
-        <div className="row gx-3">
-            {!this.state.loading && this.state.newsArticles.map((data)=>{
-                return <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3" key = {data.url}>
-                <NewsItem  title = {data.title} description = {data.description} imgUrl = {data.urlToImage} newsUrl = {data.url} mode = {mode} author = {data.author} date = {data.publishedAt}/>
-                </div>
-            })}
+        <InfiniteScroll
+          dataLength={this.state.newsArticles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<Spinner/>}
+        >
+            <div className="container">
+                <div className="row gx-3">
+                    {this.state.newsArticles.map((data,index)=>{
+                        return <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3" key = {index}>
+                        <NewsItem  title = {data.title} description = {data.description} imgUrl = {data.urlToImage} newsUrl = {data.url} mode = {mode} author = {data.author} date = {data.publishedAt}/>
+                        </div>
+                    })}
+                    </div>
             </div>
-        <div className="container my-3 d-flex justify-content-between">
-        <button type="button" disabled = {this.state.page<=1?true:false} className={`btn btn-${mode === "light" ? "primary" : "dark"}`} onClick = {this.previousClick}>&larr; Previous</button>
-        <button type="button" disabled = {this.state.page+1>Math.ceil(this.state.totalResults/this.state.pageSize)} className={`btn btn-${mode === "light" ? "primary" : "dark"}`} onClick = {this.nextClick}>Next &rarr;</button>
-        </div>
-     </div>
+            </InfiniteScroll>
+            </>
     )
   }
 }
