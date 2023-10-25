78aef2b02a9531c81f3d27c14497528828b056ab
pradHN1009 <pradyumnahn@gmail.com>
2023-10-25 21:37:40 +0530

    Refactored entire app to fucntion based components

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,49 @@
 import "./App.css";
 import Navbar from "./components/Navbar"
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import News from "./components/News";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-    apiKey = process.env.REACT_APP_API_KEY
-    constructor(){
-        super();
-        this.state = {
-            mode : "light",
-            country : "in",
-            progress : 0
-        }
-    }
-    toggleMode = () => {
-        if(this.state.mode === "light"){
-            this.setState({mode : "dark"})
+const App = () => {
+    const apiKey = process.env.REACT_APP_API_KEY
+    const [mode, setmode] = useState("light")
+    const [country, setcountry] = useState("in")
+    const [progress, setprogress] = useState(0)
+    const toggleMode = () => {
+        if(mode === "light"){
+            setmode("dark")
             document.body.style.backgroundColor = "#0F0F0F"
         }
         else{
-            this.setState({mode : "light"})
+            setmode("light")
             document.body.style.backgroundColor = "white"
         }
     }
-    setProgress = (progress) => {
-        this.setState({progress : progress})
-    }
-  render() {
     return (
     <div>
         <BrowserRouter>
-        <Navbar mode = {this.state.mode} toggleFunc = {this.toggleMode}/>
+        <Navbar mode = {mode} toggleFunc = {toggleMode}/>
         <LoadingBar
-        color={this.state.mode === 'light' ? '#0d6efd' : 'white'}
-        progress={this.state.progress}
-        onLoaderFinished={() => this.setProgress(0)}
+        color={mode === 'light' ? '#0d6efd' : 'white'}
+        progress={progress}
+        onLoaderFinished={() => setprogress(0)}
       />
         <div className="container my-3">
             <Routes>
-                <Route exact path="/" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="general" mode = {this.state.mode} country = {this.state.country} category = "general" />}/> 
-                <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="general" mode = {this.state.mode} country = {this.state.country} category = "general" />}/>
-                <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="business" mode = {this.state.mode} country = {this.state.country} category = "business" />}/>
-                <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="entertainment" mode = {this.state.mode} country = {this.state.country} category = "entertainment" />}/>
-                <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="health" mode = {this.state.mode} country = {this.state.country} category = "health" />}/>
-                <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="science" mode = {this.state.mode} country = {this.state.country} category = "science" />}/>
-                <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="sports" mode = {this.state.mode} country = {this.state.country} category = "sports" />}/>
-                <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key="technology" mode = {this.state.mode} country = {this.state.country} category = "technology" />}/>
+                <Route exact path="/" element={<News setProgress={setprogress} apiKey = {apiKey} key="general" mode = {mode} country = {country} category = "general" />}/> 
+                <Route exact path="/general" element={<News setProgress={setprogress} apiKey = {apiKey} key="general" mode = {mode} country = {country} category = "general" />}/>
+                <Route exact path="/business" element={<News setProgress={setprogress} apiKey = {apiKey} key="business" mode = {mode} country = {country} category = "business" />}/>
+                <Route exact path="/entertainment" element={<News setProgress={setprogress} apiKey = {apiKey} key="entertainment" mode = {mode} country = {country} category = "entertainment" />}/>
+                <Route exact path="/health" element={<News setProgress={setprogress} apiKey = {apiKey} key="health" mode = {mode} country = {country} category = "health" />}/>
+                <Route exact path="/science" element={<News setProgress={setprogress} apiKey = {apiKey} key="science" mode = {mode} country = {country} category = "science" />}/>
+                <Route exact path="/sports" element={<News setProgress={setprogress} apiKey = {apiKey} key="sports" mode = {mode} country = {country} category = "sports" />}/>
+                <Route exact path="/technology" element={<News setProgress={setprogress} apiKey = {apiKey} key="technology" mode = {mode} country = {country} category = "technology" />}/>
             </Routes>
             </div>
         </BrowserRouter>
     </div>
     )
-  }
 }
+
+export default App
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,95 +1,108 @@
-import React, {useEffect, useState} from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
+const News = (props) => {
+  const [newsArticles, setnewsArticles] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [page, setpage] = useState(1);
+  const [pageSize, setpageSize] = useState(20);
+  const [totalResults, settotalResults] = useState(0);
+  const [hasMore, sethasMore] = useState(true);
+  const { setProgress, apiKey, mode, country, category } = props;
 
-const News = (props) =>  {
-const [newsArticles, setnewsArticles] = useState([])
-const [loading, setloading] = useState(false)
-const [page, setpage] = useState(1)
-const [pageSize, setpageSize] = useState(20)
-const [totalResults, settotalResults] = useState(0)
-const [hasMore, sethasMore] = useState(true)
-const {setProgress, apiKey, mode, country, category} = props;
+  const capitalize = (word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  };
 
-
-
-const capitalize = (word) => {
-    return (word[0].toUpperCase() + word.slice(1))
-}
-
-document.title = `Newstle - ${capitalize(category)}`
-const fetchMoreData = async () => {
-    setpage(page+1)
+  document.title = `Newstle - ${capitalize(category)}`;
+  const fetchMoreData = async () => {
+    setpage(page + 1);
     let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data)
-    setnewsArticles(newsArticles.concat(data.articles))
-    settotalResults(data.totalResults)
-    sethasMore(newsArticles.length < totalResults)
-}
+    console.log(data);
+    setnewsArticles(newsArticles.concat(data.articles));
+    settotalResults(data.totalResults);
+    sethasMore(newsArticles.length < totalResults);
+  };
 
-const updateNews = async () => {
-    setProgress(10)
+  const updateNews = async () => {
+    setProgress(10);
     let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
-    setloading(true)
-    setProgress(20)
+    setloading(true);
+    setProgress(20);
     let response = await fetch(url);
-    setProgress(40)
+    setProgress(40);
     let data = await response.json();
-    setProgress(70)
-    setnewsArticles(data.articles)
-    settotalResults(data.totalResults)
-    setloading(false)
-    setProgress(100)
-}
-useEffect(() => {
-  updateNews()
-}, [])
+    setProgress(70);
+    setnewsArticles(data.articles);
+    settotalResults(data.totalResults);
+    setloading(false);
+    setProgress(100);
+  };
 
+  useEffect(() => {
+    updateNews();
+  }, []);
 
-
-    return (
-      <>
-        <div className={`container my-3 text-${mode === "light" ? "dark" : "light"}`}>
-            <h1 className= "text-center" style={{margin : "35px"}}>Top {capitalize(category)} Headlines</h1>
+  return (
+    <>
+      <div
+        className={`container my-3 text-${mode === "light" ? "dark" : "light"}`}
+      >
+        <h1 className="text-center" style={{ margin: "35px" }}>
+          Top {capitalize(category)} Headlines
+        </h1>
+      </div>
+      <InfiniteScroll
+        dataLength={newsArticles.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row gx-3">
+            {newsArticles.map((data, index) => {
+              return (
+                <div
+                  className="col-sm-12 col-md-6 col-lg-4 col-xl-3"
+                  key={index}
+                >
+                  <NewsItem
+                    title={data.title}
+                    description={data.description}
+                    imgUrl={data.urlToImage}
+                    newsUrl={data.url}
+                    mode={mode}
+                    author={data.author}
+                    date={data.publishedAt}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <InfiniteScroll
-          dataLength={newsArticles.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<Spinner/>}
-        >
-            <div className="container">
-                <div className="row gx-3">
-                    {newsArticles.map((data,index)=>{
-                        return <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3" key = {index}>
-                        <NewsItem  title = {data.title} description = {data.description} imgUrl = {data.urlToImage} newsUrl = {data.url} mode = {mode} author = {data.author} date = {data.publishedAt}/>
-                        </div>
-                    })}
-                    </div>
-            </div>
-            </InfiniteScroll>
-            </>
-    )
-}
+      </InfiniteScroll>
+    </>
+  );
+};
 
 News.defaultProps = {
-    setProgress : () => {},
-    apiKey : "",
-    mode : "light",
-    country : "in",
-    category : "general"
-}
+  setProgress: () => {},
+  apiKey: "",
+  mode: "light",
+  country: "in",
+  category: "general",
+};
 News.propTypes = {
-    setProgress : PropTypes.func,
-    apiKey : PropTypes.string,
-    mode : PropTypes.string,
-    country : PropTypes.string,
-    categiry : PropTypes.string
-}
+  setProgress: PropTypes.func,
+  apiKey: PropTypes.string,
+  mode: PropTypes.string,
+  country: PropTypes.string,
+  categiry: PropTypes.string,
+};
 
-export default News
+export default News;
