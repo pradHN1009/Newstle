5b0f60f977b33f1727768b8ede51ef9b98ec26f2
pradHN1009 <pradyumnahn@gmail.com>
2023-10-25 20:51:47 +0530

    Changed Navbar from class based to fucntion based components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
-export default class Navbar extends Component {
-    static defaultProps = {
-        mode : "light",
-        toggleFunc : () => {}
-    }
-    static propTypes = {
-        mode : PropTypes.string,
-        toggleFunc : PropTypes.func
-    }
-    handleChange = (event) => {
-        this.setState({query : event.target.value})
-    }
-  render() {
-    let {mode , toggleFunc} = this.props;
+const Navbar= (props) => {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" id = "navbar" data-bs-theme = {mode}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" id = "navbar" data-bs-theme = {props.mode}>
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/"><h4 style = {{marginTop : "7px"}}>Newstle</h4></NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,20 +14,31 @@ export default class Navbar extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} aria-current="page" to="/">General</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/business">Business</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/entertainment">Entertainment</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/health">Health</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/science">Science</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/sports">Sports</NavLink></li>
-                    <li className="nav-item"><NavLink className={`nav-link ${mode === 'light' ? 'light' : 'dark'}`} to="/technology">Technology</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} aria-current="page" to="/">General</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/business">Business</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/entertainment">Entertainment</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/health">Health</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/science">Science</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/sports">Sports</NavLink></li>
+                    <li className="nav-item"><NavLink className={`nav-link ${props.mode === 'light' ? 'light' : 'dark'}`} to="/technology">Technology</NavLink></li>
                 </ul>
-                <input type="checkbox" className="btn-check" id="btn-check-4" autoComplete="off" onClick={toggleFunc}/>
-                <label className={`btn btn-${mode === "light" ? "primary" : "dark"}`} htmlFor="btn-check-4">{mode === "light" ? "Dark" : "Light"} Mode</label>
+                <input type="checkbox" className="btn-check" id="btn-check-4" autoComplete="off" onClick={props.toggleFunc}/>
+                <label className={`btn btn-${props.mode === "light" ? "primary" : "dark"}`} htmlFor="btn-check-4">{props.mode === "light" ? "Dark" : "Light"} Mode</label>
                 </div>
             </div>
             </nav>
       </div>
     )
-  }
 }
+
+Navbar.propTypes = {
+    mode : PropTypes.string,
+    toggleFunc : PropTypes.func
+}
+
+Navbar.defaultProps = {
+    mode : "light",
+    toggleFunc : () => {}
+}
+
+export default Navbar
